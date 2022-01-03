d65d1ec3f6729d5d9403c2f3a62ab1beaecf2677
Wojtek Brzuszkiewicz <wbrzuszkiewicz@incuvo.com>
2022-01-03 20:11:10 +0100

    Loading Backend Config

--- Assets/Brzusko/Scripts/ConfigLoader.cs
+++ Assets/Brzusko/Scripts/ConfigLoader.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class ConfigLoader : MonoBehaviour
@@ -10,28 +11,52 @@ public class ConfigLoader : MonoBehaviour
     private string _backendConfigFileName;
 
     [SerializeField]
-    private string _credentialsFileName;
+    private BackendConfigSO _backendConfig;
 
     private string _cofnigDir;
-    private string _credentialsDir;
 
     private void Awake()
     {
-        _cofnigDir = Path.Combine(Application.dataPath, "config");
-        _credentialsDir = Path.Combine(Application.dataPath, "credentials");
-        Debug.Log(_credentialsDir);
+        _cofnigDir = Path.Combine(@Application.dataPath, "config");
     }
 
     private void Start()
     {
         CreateFilesIfNotExist();
+        LoadConfig();
     }
 
     private void CreateFilesIfNotExist()
     {
         if (!Directory.Exists(_cofnigDir))
             Directory.CreateDirectory(_cofnigDir);
-        if (!Directory.Exists(_credentialsDir))
-            Directory.CreateDirectory(_credentialsDir);
+
+        var configPath = Path.Combine(_cofnigDir, _backendConfigFileName);
+  
+        if (!File.Exists(configPath))
+        {
+            var configData = JsonUtility.ToJson(new BackendConfigFileData());
+            
+            using(var fileStream = File.Create(configPath))
+            using (var writer = new StreamWriter(fileStream))
+            {
+                writer.Write(configData);
+            }
+        }
+    }
+
+    private void LoadConfig()
+    {
+        BackendConfigFileData fileData;
+        var configPath = Path.Combine(_cofnigDir, _backendConfigFileName);
+        using(var fs = File.OpenRead(configPath))
+        using(var reader = new StreamReader(fs))
+        {
+            fileData = JsonUtility.FromJson<BackendConfigFileData>(reader.ReadToEnd());
+        }
+        _backendConfig.AccountURL = fileData.AccountURL;
+        _backendConfig.AuthURL = fileData.AuthURL;
+        _backendConfig.PositionURL = fileData.PositionURL;
+        _backendConfig.VisualsURL = fileData.VisualsURL;
     }
 }
--- Assets/Brzusko/Scripts/Scriptables/BackendConfigSO.cs
+++ Assets/Brzusko/Scripts/Scriptables/BackendConfigSO.cs
@@ -4,8 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "BackendConfig", menuName = "Configs/BackendConfig")]
+public class BackendConfigSO : ScriptableObject
+{
+    public string AuthURL;
+    public string AccountURL;
+    public string VisualsURL;
+    public string PositionURL;
+}
+
 [Serializable]
-public class ConfigSO : ScriptableObject
+public class BackendConfigFileData
 {
     public string AuthURL;
     public string AccountURL;
