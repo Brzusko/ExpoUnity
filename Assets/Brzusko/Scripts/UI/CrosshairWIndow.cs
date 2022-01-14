f984db91c7a0d646fa14c4d462a7f0a1d46a5b2b
Wojciech Brzusko Brzuszkiewicz <wojciech.brzuszkiewicz@hotmail.com>
2022-01-14 01:08:55 +0100

    Added crosshair

---
+++ Assets/Brzusko/Scripts/UI/CrosshairWIndow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairWIndow : Window
+{
+    [SerializeField]
+    private Color _baseColor;
+    [SerializeField]
+    private Color _targetColor;
+
+
+    private void Start()
+    {
+
+    }
+}
